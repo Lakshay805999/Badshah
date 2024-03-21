ec874e66f00c3360beca550c34109417a44b4d56
Lakshay805999 <164336227+Lakshay805999@users.noreply.github.com>
2024-03-21 16:36:09 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,13 +6,13 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "20389440"))
-    API_HASH = getenv("API_HASH", "a1a06a18eb9153e9dbd447cfd5da2457")
-    BOT_TOKEN = getenv("BOT_TOKEN", "6564513574:AAGDqUaEmeu0m4DjLDetNc4nooVTWYT7Fzo")
-    FSUB = getenv("FSUB", "VJ_Botz")
-    CHID = int(getenv("CHID", "-1001623633000"))
-    SUDO = list(map(int, getenv("SUDO", "6168162777").split()))
-    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://sushankm16:4i1WAfPYKWyqPIDD@cluster0.sngp9pz.mongodb.net/?retryWrites=true&w=majority")
+    API_ID = int(getenv("API_ID", "25875005"))
+    API_HASH = getenv("API_HASH", "51bb01f4b3c9166f8d60c2811c941074")
+    BOT_TOKEN = getenv("BOT_TOKEN", "7039775021:AAF2RZLvwS1LIRYS7w-TELxeclkqodkfd80")
+    FSUB = getenv("FSUB", "@Badshah_Req_Bot")
+    CHID = int(getenv("CHID", "--1001715384088"))
+    SUDO = list(map(int, getenv("SUDO", "815165836").split()))
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://fantasybadsha:<zmpvPWFiONCU8xVv>@serverlessinstance0.okk83ih.mongodb.net/")
     
 cfg = Config()
 
